bdf83db0fd0451feded03dfd4fd23039bf055d13
abdullahwale <abdullahwale@gmail.com>
2020-12-28 12:29:37 +0500

    Multiple Screens Added and and SOme Animations

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:new_chat_app_complete/main.dart';
+import 'package:new_chat_app_complete/screens/welcome_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(WelcomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
